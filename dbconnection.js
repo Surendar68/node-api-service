c0907cde3864c524b81b4ddb4b374dc665ae7043
Surendar68 <surendarcodez@gmail.com>
2023-02-28 11:15:06 +0530

    db connect commet

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'dbadmin',
-    password: 'Admin@123#!',
-    database: 'node-database'
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'dbadmin',
+//     password: 'Admin@123#!',
+//     database: 'node-database'
+// });
 
 module.exports = connection;
